7017333f9f3a95c3f248ff29b35c1f57b8b6c6c1
唐戸渉太 <shotakarato@shotakarato-2.local>
2021-10-25 11:39:18 +0900

    setup: fixed the file structure for deployment

--- halfway-review/test.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE notes (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(150) NOT NULL,
-  content VARCHAR(250) NOT NULL
-);
-
-INSERT INTO notes (title, content)
-VALUES ('today', 'This is dummy data 1'),
-('tomorrow', 'This is dummy data 2');
