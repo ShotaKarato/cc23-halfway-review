f4faa99a15516e25b269de5fba5b697699b238e3
唐戸渉太 <shotakarato@shotakarato-2.local>
2021-10-25 11:42:59 +0900

    setup: fixed file structure for deployment

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const knex = require("./knex");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+app.use(express.json());
+
+app.get("/notes", async (req, res) => {
+  let data = await knex().select().table("notes");
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server is running on port ${port}`);
+});
